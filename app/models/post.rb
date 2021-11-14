db7a52d94e533304ee31fc45ada4d46f7e7f0ace
Jarad Solomon <jaradsolomon@gmail.com>
2021-11-14 16:35:48 -0600

    commit tree 31c8a90b3350c0fd5f2a49e30e31253ff50bfd2d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :is_clickbait?
+
+# def either_or
+#     unless category.match?("Fiction") || category.match?("Non-Fiction")
+#         errors.add(:category,"Nope")
+#     end
+# end
+
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+def is_clickbait? 
+    if CLICKBAIT_PATTERNS.none? { |it| it.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
 end
